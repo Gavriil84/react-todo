c08664661f644882b338b054b9b0fd787c21cf2f
Gavriil84 <79692322+Gavriil84@users.noreply.github.com>
2021-08-25 22:10:37 -0700

    add usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [uncompletedTodos, setUncompletedTodos] = useState([
+    "Something1",
+    "Something2"
+  ]);
+  const [completedTodos, setCompletedTodos] = useState(["Something3"]);
   return (
     <>
       <div className="input-area">
@@ -11,25 +16,28 @@ export const App = () => {
       <div className="uncompleted-area">
         <p className="title">Uncompleted Todo</p>
         <ul>
-          <div className="list-row">
-            <li>Something</li>
-            <button>Complete</button>
-            <button>Delete</button>
-          </div>
-          <div className="list-row">
-            <li>Something</li>
-            <button>Complete</button>
-            <button>Delete</button>
-          </div>
+          {uncompletedTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>Add</button>
+                <button>Delete</button>
+              </div>
+            );
+          })}
         </ul>
       </div>
       <div className="complete-area">
         <p className="title">Completed Todo</p>
         <ul>
-          <div className="list-row">
-            <li>Something</li>
-            <button>Back</button>
-          </div>
+          {completedTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>Back</button>
+              </div>
+            );
+          })}
         </ul>
       </div>
     </>
