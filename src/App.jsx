cabaa5b8a0dd76606d580e31f2de420e99620a7a
Gavriil84 <79692322+Gavriil84@users.noreply.github.com>
2021-08-26 17:35:41 -0700

    small adjustment

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,31 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [todoText, setTodotext] = useState("");
   const [uncompletedTodos, setUncompletedTodos] = useState([
     "Something1",
     "Something2"
   ]);
   const [completedTodos, setCompletedTodos] = useState(["Something3"]);
+
+  const onChangeTodoText = (event) => setTodotext(event.target.value);
+
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...uncompletedTodos, todoText];
+    setUncompletedTodos(newTodos);
+    setTodotext("");
+  };
+
   return (
     <>
       <div className="input-area">
-        <input placeholder="Enter the todo" />
-        <button>Add</button>
+        <input
+          placeholder="Enter the todo"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>Add</button>
       </div>
       <div className="uncompleted-area">
         <p className="title">Uncompleted Todo</p>
